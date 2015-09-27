9e04eb462c34be039992317a8612f6608c176f45
dance2die <dance2die@hotmail.com>
2015-09-27 14:28:13 -0400

    First attemp for async/await.

---
+++ Demo.AsyncTest/Demo.AsyncTest.AsyncAwait/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Nito.AsyncEx;
+
+namespace Demo.AsyncTest.AsyncAwait
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			var responseString = AsyncContext.Run(GetResponseString);
+
+			Console.WriteLine(responseString);
+
+			Console.Read();
+		}
+
+		private async static Task<string> GetResponseString()
+		{
+			var requestUri = new Uri("http://www.google.com");
+			using (var client = new HttpClient())
+			{
+				var result = await client.GetStringAsync(requestUri);
+				return result;
+			}
+		}
+	}
+}
