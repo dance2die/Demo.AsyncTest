68778ba4c8a2cca5180ae22375bf150296d6bd8e
dance2die <dance2die@hotmail.com>
2015-10-04 18:35:33 -0400

    async using WebClient

--- Demo.AsyncTest/Demo.AsyncTest.AsyncDownloadForm/MainForm.cs
+++ Demo.AsyncTest/Demo.AsyncTest.AsyncDownloadForm/MainForm.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -19,16 +20,28 @@ namespace Demo.AsyncTest.AsyncDownloadForm
 
 		private async void downloadButton_Click(object sender, EventArgs e)
 		{
+			const string uri = "http://www.huffingtonpost.com/feeds/index.xml";
+
 			resultTextBox.Text = string.Empty;
 
-			// http://www.dotnetperls.com/httpclient
-			using (var client = new HttpClient())
-			using (HttpResponseMessage responseMessage = await client.GetAsync("http://www.huffingtonpost.com/feeds/index.xml"))
-			using (HttpContent content = responseMessage.Content)
+			//// http://www.dotnetperls.com/httpclient
+			//using (var client = new HttpClient())
+			//{
+			//	using (HttpResponseMessage responseMessage = await client.GetAsync(uri))
+			//	using (HttpContent content = responseMessage.Content)
+			//	{
+			//		await Task.Delay(3000);
+			//		string result = await content.ReadAsStringAsync();
+			//		resultTextBox.Text = result;
+			//	}
+			//}
+
+			using (var webClient = new WebClient())
 			{
-				await Task.Delay(3000);
-				string result = await content.ReadAsStringAsync();
-				resultTextBox.Text = result;
+				Task<string> task = webClient.DownloadStringTaskAsync(uri);
+				await Task.Delay(4000);
+				string value =await task;
+				resultTextBox.Text = value;
 			}
 		}
 	}
