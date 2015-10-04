c2cdf64c73e7c2afe234334050130b689f80f83f
dance2die <dance2die@hotmail.com>
2015-10-04 17:26:34 -0400

    added a comment.

---
+++ Demo.AsyncTest/Demo.AsyncTest.AsyncDownloadForm/MainForm.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net.Http;
+using System.Windows.Forms;
+
+namespace Demo.AsyncTest.AsyncDownloadForm
+{
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			InitializeComponent();
+		}
+
+		private void closeButton_Click(object sender, EventArgs e)
+		{
+			Close();
+		}
+
+		private async void downloadButton_Click(object sender, EventArgs e)
+		{
+			// http://www.dotnetperls.com/httpclient
+			using (var client = new HttpClient())
+			using (HttpResponseMessage responseMessage = await client.GetAsync("http://www.huffingtonpost.com/feeds/index.xml"))
+			using (HttpContent content = responseMessage.Content)
+			{
+				string result = await content.ReadAsStringAsync();
+				resultTextBox.Text = result;
+			}
+		}
+	}
+}
