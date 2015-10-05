1d76636c2d42c9a7d263b1317515d67da9812273
dance2die <dontbothermewithspam@gmail.com>
2015-10-05 15:50:01 -0400

    added remark where to get more info

--- Demo.AsyncTest/Demo.AsyncTest.AsyncDownloadForm/WebRequestTestForm.cs
+++ Demo.AsyncTest/Demo.AsyncTest.AsyncDownloadForm/WebRequestTestForm.cs
@@ -22,6 +22,13 @@ namespace Demo.AsyncTest.AsyncDownloadForm
 			Close();
 		}
 
+		/// <remarks>
+		/// Create asynchronous versions of WebRequest/Response methods using following links.
+		/// <see cref="https://www.google.com/webhp?sourceid=chrome-instant&ion=1&espv=2&es_th=1&ie=UTF-8#q=task.factory.fromasync%20webrequest&es_th=1"/>
+		/// <see cref="http://stackoverflow.com/a/23004036/4035"/>
+		/// <see cref="http://stackoverflow.com/a/14098942/4035"/>
+		/// <see cref="http://stackoverflow.com/q/17664191/4035"/>
+		/// </remarks>
 		private async void processButton_Click(object sender, EventArgs e)
 		{
 
